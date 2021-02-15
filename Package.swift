d54bf7f5efd6791d28e79a7dff1042a80e4b9867
Alexey Troshkov <alexey.troshkov@piano.io>
2021-02-15 14:17:32 +0300

    - Fix Package.swift;

--- Package.swift
+++ Package.swift
@@ -7,8 +7,13 @@ let package = Package(
         .library(
             name: "CxenseSDK",
             targets: [
-                "CxenseSDK",
-                "CxenseSDKTv"
+                "CxenseSDK"
+            ]
+        ),
+        .library(
+            name: "CxenseSDK_TV",
+            targets: [
+                "CxenseSDK_TV"
             ]
         )
     ],
@@ -20,7 +25,7 @@ let package = Package(
             checksum: "6ea29da04a6f58a78e320575c3520293815c20feac58a669e0c2daf95d49d045"
         ),
         .binaryTarget(
-            name: "CxenseSDKTv",
+            name: "CxenseSDK_TV",
             url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-tvOS-1.9.1.zip",
             checksum: "ec010c56f31900338a5f12c09e8b331eee72a2aaa457aa0fb27f8775d77f2284"
         )
